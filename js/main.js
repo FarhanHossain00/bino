cc7f9ad17bdd8dda369c28cb4e19fd7254005f01
FarhanHossain00 <49363515+FarhanHossain00@users.noreply.github.com>
2019-07-13 01:15:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$(document).ready(function () {
+
+
+//preloader
+
+$('.card').delay(2600)
+.fadeOut()
+.queue(function() {
+  $('.preloader').addClass("close").fadeOut(3000);
+  $('.preloader').dequeue();
+});
+
+//navbar
+$('.navbar .navbar-nav li a').click(function () {
+  $('.navbar .navbar-collapse').removeClass('show')
+})
+
+  //==============================
+  // smooth scroll
+  //==============================
+  $(".navbar-nav li a[href^='#']").on("click", function (e) {
+    e.preventDefault();
+    var hash = this.hash;
+    $('html, body').animate({
+      scrollTop: $(hash).offset().top
+    }, 1000);
+
+  });
+
+
+  //projects
+
+$('.project-menu li').click(function(){
+  $('.project-menu li').removeClass('active')
+  $(this).addClass('active')
+})
+
+    //==============================
+    //on-scroll
+    //==============================
+    $(window).on('scroll', function () {
+      var wscroll = $(this).scrollTop();
+      //fixed-nav
+      wscroll > 1 ? $("#site-header").addClass("fixed") : $("#site-header").removeClass("fixed");
+  
+      // go top appear
+      wscroll > 700 ? $("#goTop").fadeIn() : $("#goTop").fadeOut();
+  
+    });
+  
+    //go top
+    $("#goTop").click(function () {
+      $("body, html").animate({
+        scrollTop: 0
+      }, 600);
+    });
+  
+  
+  
+
+    $('#btn1').hover(function(){
+        $(this).removeClass('hmm')
+         $("#btn2").addClass('hmm')
+         $('#btn2').toggleClass('shrink')
+
+    });
+
+    $('#btn2').hover(function(){
+        $(this).removeClass('hmm')
+         $("#btn1").addClass('hmm')
+         $('#btn1').toggleClass('shrink')
+    });
+    $(".go-down").click(function() {
+        $('html, body').animate({
+            scrollTop: $("#About").offset().top
+        }, 2000, );
+    });
+
+
+    //mix it up
+    var mixer = mixitup('.img-gallery');
+
+    //price slide
+    $('#price-slide').owlCarousel({
+      items: 3,
+      autoplay:false,
+      margin:50,
+      responsive:{
+        0:{items:1},
+        768:{items:2},
+        992:{items:3}
+      }
+    });
+});
