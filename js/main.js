a39466e7a6fd4459f9b65369d96058e7b3ec1fb1
FarhanHossain00 <49363515+FarhanHossain00@users.noreply.github.com>
2019-07-13 05:28:54 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 $(document).ready(function () {
 
 
-//preloader
+  //preloader
+  $(window).on("load", function () {
 
-$('.card').delay(2600)
-.fadeOut()
-.queue(function() {
-  $('.preloader').addClass("close").fadeOut(3000);
-  $('.preloader').dequeue();
-});
+    $('.card').delay(600).fadeOut();
+    $('.preloader').addClass('close').delay(3000).fadeOut();
+  });
 
-//navbar
-$('.navbar .navbar-nav li a').click(function () {
-  $('.navbar .navbar-collapse').removeClass('show')
-})
+  //navbar
+  $('.navbar .navbar-nav li a').click(function () {
+    $('.navbar .navbar-collapse').removeClass('show')
+  })
 
   //==============================
   // smooth scroll
@@ -30,65 +28,71 @@ $('.navbar .navbar-nav li a').click(function () {
 
   //projects
 
-$('.project-menu li').click(function(){
-  $('.project-menu li').removeClass('active')
-  $(this).addClass('active')
-})
+  $('.project-menu li').click(function () {
+    $('.project-menu li').removeClass('active')
+    $(this).addClass('active')
+  })
 
-    //==============================
-    //on-scroll
-    //==============================
-    $(window).on('scroll', function () {
-      var wscroll = $(this).scrollTop();
-      //fixed-nav
-      wscroll > 1 ? $("#site-header").addClass("fixed") : $("#site-header").removeClass("fixed");
-  
-      // go top appear
-      wscroll > 700 ? $("#goTop").fadeIn() : $("#goTop").fadeOut();
-  
-    });
-  
-    //go top
-    $("#goTop").click(function () {
-      $("body, html").animate({
-        scrollTop: 0
-      }, 600);
-    });
-  
-  
-  
+  //==============================
+  //on-scroll
+  //==============================
+  $(window).on('scroll', function () {
+    var wscroll = $(this).scrollTop();
+    //fixed-nav
+    wscroll > 1 ? $("#site-header").addClass("fixed") : $("#site-header").removeClass("fixed");
 
-    $('#btn1').hover(function(){
-        $(this).removeClass('hmm')
-         $("#btn2").addClass('hmm')
-         $('#btn2').toggleClass('shrink')
+    // go top appear
+    wscroll > 700 ? $("#goTop").fadeIn() : $("#goTop").fadeOut();
 
-    });
+  });
 
-    $('#btn2').hover(function(){
-        $(this).removeClass('hmm')
-         $("#btn1").addClass('hmm')
-         $('#btn1').toggleClass('shrink')
-    });
-    $(".go-down").click(function() {
-        $('html, body').animate({
-            scrollTop: $("#About").offset().top
-        }, 2000, );
-    });
+  //go top
+  $("#goTop").click(function () {
+    $("body, html").animate({
+      scrollTop: 0
+    }, 600);
+  });
 
 
-    //mix it up
-    var mixer = mixitup('.img-gallery');
 
-    //price slide
-    $('#price-slide').owlCarousel({
-      items: 3,
-      autoplay:false,
-      margin:50,
-      responsive:{
-        0:{items:1},
-        768:{items:2},
-        992:{items:3}
+
+  $('#btn1').hover(function () {
+    $(this).removeClass('hmm')
+    $("#btn2").addClass('hmm')
+    $('#btn2').toggleClass('shrink')
+
+  });
+
+  $('#btn2').hover(function () {
+    $(this).removeClass('hmm')
+    $("#btn1").addClass('hmm')
+    $('#btn1').toggleClass('shrink')
+  });
+  $(".go-down").click(function () {
+    $('html, body').animate({
+      scrollTop: $("#About").offset().top
+    }, 2000, );
+  });
+
+
+  //mix it up
+  var mixer = mixitup('.img-gallery');
+
+  //price slide
+  $('#price-slide').owlCarousel({
+    items: 3,
+    autoplay: false,
+    margin: 50,
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
       }
-    });
+    }
+  });
 });
